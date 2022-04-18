93989968d3e26766825803ad7327b0f8098e6a7d
ENMANUEL URENA VARGAS <1095352@est.intec.edu.do>
2022-04-18 16:14:09 -0400

    Web app .net framework

---
+++ TodoListNetWeb/Controllers/ListController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BusinessObject;
+
+namespace TodoListNetWeb.Controllers
+{
+    public class ListController : Controller
+    {
+        TodoListDBEntities db = new TodoListDBEntities();
+        // GET: List
+        public ActionResult Index()
+        {
+            var Lists = db.Lists.ToList();
+            return View(Lists);
+        }
+    }
+}
---
+++ TodoListNetWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TodoListNetWeb.MvcApplication" Language="C#" %>
