7ef199cf710e34c285f91725477227a933ecddec
ENMANUEL URENA VARGAS <1095352@est.intec.edu.do>
2022-04-21 16:52:01 -0400

    Muchos bugs pero funciona

--- TodoListDesktop/AppWindow.xaml.cs
+++ TodoListDesktop/AppWindow.xaml.cs
@@ -21,6 +21,7 @@ namespace TodoListDesktop
     /// </summary>
     public partial class AppWindow : Window
     {
+        public static Lists selectedList = new Lists();
         public AppWindow()
         {
             InitializeComponent();
@@ -30,14 +31,9 @@ namespace TodoListDesktop
 
         private void CargarLista()
         {
-            var UserList = MainWindow.logedUser.Lists.ToList();
-            List<string> list = new List<string>();
-            foreach (var item in UserList)
-            {
-                list.Add(item.Name);
-            }
-            list.Reverse();
-            ListBox_MyList.ItemsSource = list;
+            ListBox_MyList.ItemsSource = null;
+            var UserList = MainWindow.logedUser.Lists;
+            ListBox_MyList.ItemsSource = UserList;
         }
 
         private void Btn_Home_Click(object sender, RoutedEventArgs e)
@@ -79,6 +75,65 @@ namespace TodoListDesktop
             newList.Users = MainWindow.logedUser;
             MainWindow.db.Lists.Add(newList);
             MainWindow.db.SaveChanges();
+        }
+
+        private void ListBox_MyList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {            
+            var curItemIndex = ListBox_MyList.SelectedIndex;
+            var UserList = MainWindow.logedUser.Lists.ToList();
+            try
+            {
+                selectedList = UserList[curItemIndex];
+                Products products = new Products();
+                products.Show();
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void Btn_DeleteList_Click(object sender, RoutedEventArgs e)
+        {
+            Button cmd = (Button)sender;
+            var list = cmd.DataContext;
+            bool erased = false;
+            try
+            {
+                MainWindow.db.Lists.Remove((Lists)list);
+                MainWindow.db.SaveChanges();
+                erased = true;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("No se puede eliminar una lista con productos, primero elimine los productos dentro de la lista", "Delete Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            if (erased)
+            {
+                CargarLista();
+            }
+        }
+
+        private void Btn_UpdateList_Click(object sender, RoutedEventArgs e)
+        {
+            Button cmd = (Button)sender;
+            bool updated = false;
+            try
+            {
+                var updateList = (Lists)cmd.DataContext;
+                var list = MainWindow.db.Lists.Single(a => a.ListID == updateList.ListID);
+                list.Name = TextBox_NewList.Text;
+                MainWindow.db.SaveChanges();
+                updated = true;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("No se ha podido modificar la lista", "Update Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            if (updated)
+            {
+                CargarLista();
+            }
         }
     }
 }
---
+++ TodoListDesktop/Products.xaml.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using BusinessObject;
+
+namespace TodoListDesktop
+{
+    /// <summary>
+    /// Interaction logic for Products.xaml
+    /// </summary>
+    public partial class Products : Window
+    {
+        private Lists ThisList = new Lists();
+        private int productquantity;
+ 
+
+        public Products()
+        {
+            InitializeComponent();
+            ThisList = MainWindow.db.Lists.Find(AppWindow.selectedList.ListID);
+            Label_Name.Content = MainWindow.logedUser.Name;
+            Label_WindowTitle.Content = ThisList.Name;
+            CargarCantidad();
+            CargarProductos();
+        }
+
+        private void CargarCantidad()
+        {
+            var list = new List<int>();
+            for (int i = 1; i < 100 ; i++)
+            {
+                list.Add(i);
+            }
+            ComboBox_Quantity.ItemsSource = list;
+        }
+
+        private void Btn_AddProduct_Click(object sender, RoutedEventArgs e)
+        {
+            var name = TextBox_NewProduct.Text;
+            AñadirProducto(name);
+            TextBox_NewProduct.Text = "";
+            CargarProductos();
+        }
+
+        private void AñadirProducto(string name)
+        {
+            var newProduct = new Product();
+            newProduct.Name = name;
+            newProduct.Quantity = productquantity;
+            newProduct.Lists = ThisList;
+            MainWindow.db.Product.Add(newProduct);
+            MainWindow.db.SaveChanges();
+        }
+
+        private void CargarProductos()
+        {
+            ListBox_MyProduct.ItemsSource = null;
+            ListBox_MyProduct.ItemsSource = ThisList.Product;
+        }
+
+        private void Btn_UpdateList_Click(object sender, RoutedEventArgs e)
+        {
+            Button cmd = (Button)sender;
+            bool updated = false;
+            try
+            {
+                var updateProduct = (Product)cmd.DataContext;
+                var product = MainWindow.db.Product.Single(a => a.ProductID == updateProduct.ProductID);
+                if (TextBox_NewProduct.Text != "")
+                {
+                    product.Name = TextBox_NewProduct.Text;
+                    product.Quantity = productquantity;
+                    MainWindow.db.SaveChanges();
+                    updated = true;
+                }
+                else
+                {
+                    product.Quantity = productquantity;
+                    MainWindow.db.SaveChanges();
+                    updated = true;
+                }
+                
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("No se ha podido eliminar el producto", "Delete Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            if (updated)
+            {
+                CargarProductos();
+            }
+        }
+
+        private void Btn_DeleteProduct_Click(object sender, RoutedEventArgs e)
+        {
+            Button cmd = (Button)sender;
+            var product = cmd.DataContext;
+            bool erased = false;
+            try
+            {
+                MainWindow.db.Product.Remove((Product)product);
+                MainWindow.db.SaveChanges();
+                erased = true;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("No se ha podido modificar el producto", "Update Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            if (erased)
+            {
+                CargarProductos();
+            }
+        }
+
+        private void ComboBox_Quantity_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBox cmd = (ComboBox)sender;
+            var quantity = cmd.SelectedValue;
+            try
+            {
+               productquantity = Convert.ToInt32(quantity);
+               if (cmd.Text != null)
+               {
+                 cmd.Text = quantity.ToString();
+               }
+            }
+            catch 
+            {
+
+            }
+        }
+    }
+}
